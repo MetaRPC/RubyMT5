1d4357cca2edc037a0cfe50f53f274c7a53d0072
MetaRPC <info@metarpc.pro>
2026-09-08 04:58:52 -0700

    Add unit test suite and CI workflow

---
+++ test/test_client.rb
@@ -0,0 +1,49 @@
+﻿require "minitest/autorun"
+require_relative "../lib/metarpc_mt5"
+
+class TestMetaRPCMT5Client < Minitest::Test
+  def setup
+    @client = MetaRPC::MT5::Client.new("mt5.mrpc.pro", 443)
+  end
+
+  def test_initial_state
+    refute @client.connected?
+    assert_equal "mt5.mrpc.pro", @client.host
+    assert_equal 443, @client.port
+  end
+
+  def test_connect_and_disconnect
+    assert @client.connect(1001, "password")
+    assert @client.connected?
+
+    @client.disconnect
+    refute @client.connected?
+  end
+
+  def test_account_info
+    @client.connect(1001, "password")
+    info = @client.account_info
+    assert_equal 2005432, info[:login]
+    assert_equal "USD", info[:currency]
+    assert_equal 50000.0, info[:balance]
+    assert_equal "mt5.mrpc.pro", info[:server]
+  end
+
+  def test_order_lifecycle
+    @client.connect(1001, "password")
+    res = @client.order_send(
+      symbol: "EURUSD",
+      action: "BUY",
+      volume: 0.1,
+      price: 1.0850,
+      comment: "Test trade"
+    )
+    assert_equal 0, res[:retcode]
+    assert_equal 0.1, res[:volume]
+    assert_equal 1.0850, res[:price]
+    assert res[:ticket] > 0
+
+    assert @client.order_modify(ticket: res[:ticket], stop_loss: 1.0800)
+    assert @client.order_close(ticket: res[:ticket], volume: 0.1)
+  end
+end
